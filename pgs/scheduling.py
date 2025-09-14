6b564f6c52028559b933236592f35e6f7ac3103e
chelsea_cedric <53366054+antonie-riziki@users.noreply.github.com>
2025-09-14 05:19:11 +0000

    removed streamlit pandas profile

--- pgs/scheduling.py
+++ pgs/scheduling.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # from ydata_profiling import ProfileReport
 
-from streamlit_pandas_profiling import st_profile_report
+# from streamlit_pandas_profiling import st_profile_report
 
 from datetime import datetime
 from dotenv import load_dotenv
